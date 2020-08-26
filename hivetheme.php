3f1503f467b5f334f1bc93faaad57bd0c3e2c229
Ihor <ihor@hivepress.io>
2020-08-26 12:27:36 +0300

    Add framework files

--- hivetheme.php
+++ hivetheme.php
@@ -14,6 +14,11 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+// Define the core file.
+if ( ! defined( 'HT_FILE' ) ) {
+	define( 'HT_FILE', __FILE__ );
+}
+
 // Include the core class.
 require_once __DIR__ . '/includes/class-core.php';
 
