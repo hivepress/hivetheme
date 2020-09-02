ca528b8b87aab52ffc0ed9486aa71bd8a5465fa4
Ihor <ihor@hivepress.io>
2020-09-02 16:15:10 +0300

    Fix loader color

--- includes/components/class-template.php
+++ includes/components/class-template.php
@@ -153,7 +153,7 @@ final class Template extends Component {
 		$output = '';
 
 		// Render page loader.
-		$output .= $this->render_part( 'templates/page/page-loader', [ 'color' => '#ffc107' ] );
+		$output .= $this->render_part( 'templates/page/page-loader', [ 'color' => get_theme_mod( 'primary_color' ) ] );
 
 		// Render skip link.
 		$output .= '<a href="#content" class="skip-link screen-reader-text">' . esc_html__( 'Skip to content', 'hivetheme' ) . '</a>';
