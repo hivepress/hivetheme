b7932f503285ba8760ad2357ce338da50580619d
Ihor <ihor@hivepress.io>
2020-09-03 15:43:17 +0300

    Update version

--- hivetheme.php
+++ hivetheme.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: HiveTheme
  * Description: A framework for HivePress themes.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: HivePress
  * Author URI: https://hivepress.io/
  * Text Domain: hivetheme
