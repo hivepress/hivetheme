221d0ee668cb6385847a85c3391f23c58bb269dd
Ihor <ihor@hivepress.io>
2021-03-18 19:42:56 +0200

    Update version

--- hivetheme.php
+++ hivetheme.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: HiveTheme
  * Description: A framework for HivePress themes.
- * Version: 1.0.7
+ * Version: 1.0.8
  * Author: HivePress
  * Author URI: https://hivepress.io/
  * Text Domain: listinghive
