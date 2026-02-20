7c36157dd7913b0e07a56cc0194caccae8d17e90
HivePress <43493187+hivepress@users.noreply.github.com>
2026-02-20 17:18:49 +0100

    Add post meta box

--- includes/configs/meta-boxes.php
+++ includes/configs/meta-boxes.php
@@ -10,7 +10,7 @@ defined( 'ABSPATH' ) || exit;
 
 return [
 	'page_settings' => [
-		'title'    => esc_html__( 'Settings', 'listinghive' ),
+		'title'    => esc_html__( 'Layout', 'listinghive' ),
 		'screen'   => 'page',
 		'context'  => 'side',
 		'priority' => 'default',
@@ -18,6 +18,33 @@ return [
 		'fields'   => [
 			'hide_title'  => [
 				'label'  => esc_html__( 'Hide page title', 'listinghive' ),
+				'type'   => 'checkbox',
+				'_order' => 10,
+			],
+
+			'hide_header' => [
+				'label'  => esc_html__( 'Hide site header', 'listinghive' ),
+				'type'   => 'checkbox',
+				'_order' => 20,
+			],
+
+			'hide_footer' => [
+				'label'  => esc_html__( 'Hide site footer', 'listinghive' ),
+				'type'   => 'checkbox',
+				'_order' => 30,
+			],
+		],
+	],
+
+	'post_settings' => [
+		'title'    => esc_html__( 'Layout', 'listinghive' ),
+		'screen'   => 'post',
+		'context'  => 'side',
+		'priority' => 'default',
+
+		'fields'   => [
+			'hide_hero'   => [
+				'label'  => esc_html__( 'Hide post header', 'listinghive' ),
 				'type'   => 'checkbox',
 				'_order' => 10,
 			],
