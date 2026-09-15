ab9c723b630608255681ccb32295ff1b1ada6f86
HivePress <43493187+hivepress@users.noreply.github.com>
2026-09-15 10:22:12 +0200

    Fix loading RTL styles

--- includes/components/class-asset.php
+++ includes/components/class-asset.php
@@ -92,6 +92,10 @@ final class Asset extends Component {
 
 			if ( ht\get_array_value( $style, 'rtl' ) ) {
 				wp_style_add_data( $style['handle'], 'rtl', 'replace' );
+
+				if ( strpos( $style['src'], '.min.css' ) ) {
+					wp_style_add_data( $style['handle'], 'suffix', '.min' );
+				}
 			}
 		}
 	}
